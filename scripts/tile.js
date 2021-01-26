6185d6a4f50d9fc639267e069463de144335ef98
dzevakov <dmitryzev@gmail.com>
2021-01-27 02:09:59 +0300

    feat: add normalize.css, implement Drag'n'Drop for canvas

--- scripts/tile.js
+++ scripts/tile.js
@@ -9,7 +9,6 @@ export class Tile {
     if(this.caption === 0) {
       canvasContext.clearRect((this.X * 100), (this.Y * 100), 100, 100);
     } else {
-      canvasContext.strokeStyle = 'black';
       canvasContext.fillStyle = 'rgb(113,161,255)';
       canvasContext.fillRect((this.X * 100 + 5), (this.Y * 100 + 5), 90, 90);
       canvasContext.strokeRect((this.X * 100 + 5), (this.Y * 100 + 5), 90, 90);
